5592e4200dbeae142194ff7188b0a373824b8818
Korotchenko Arina (AK2) <yankor@bk.ru>
2016-06-08 17:42:46 +0400

    Correct some mistakes

--- T06SPHR/SPHERE.C
+++ T06SPHR/SPHERE.C
@@ -12,12 +12,31 @@ Date: 06.06.2016
 #define N 15
 #define M 30
 #define R 300
+#define PI 3.14159265357989323846
+
+typedef struct 
+{
+  DOUBLE X, Y, Z;
+} VEC;
+
+VEC Rot( VEC V, DOUBLE Angel)
+{
+  DOUBLE A = Angel * PI / 180;
+  VEC r;
+
+  r.X = V.X;
+  r.Y = V.Y * cos(A) - V.Z * sin(A);
+  r.Z = V.Y * sin(A) + V.Z * cos(A);
+  return r;
+}
 
 /*Start of drawing sphere function*/
 VOID DrawSphere( HDC hDC, INT Xc, INT Yc )
 {
   INT i, j;
-  DOUBLE theta, phi, x, y, z, phase = clock() / 5000.0;
+  DOUBLE theta, phi, X, Y, Z, phase = clock() / 5000.0;
+  static VEC G[N][M];
+
 
   SelectObject(hDC, GetStockObject(DC_PEN));
   SelectObject(hDC, GetStockObject(DC_BRUSH));
@@ -25,46 +44,57 @@ VOID DrawSphere( HDC hDC, INT Xc, INT Yc )
   SetDCBrushColor(hDC, RGB(00, 00, 00));
   for (i = 0; i <= N; i++ )
   {
-    theta = i * 3.14159265358979323846 / N;    
+    theta = i * PI / N;    
     for (j = 0; j <= M; j++)
     {
-      phi = j * 2 * 3.14159265358979323846 / M + phase;
-      x = Xc + R * sin(theta) * cos(phi);
-      y = Yc - R * cos(theta);
-      z = R * sin(theta) * cos(phi);
-      Ellipse(hDC, x - 5, y - 5, x + 5, y + 5);
+      phi = j * 2 * PI / M + phase;
+      G[i][j].X = R * sin(theta) * sin(phi);
+      G[i][j].Y = R * cos(theta);
+      G[i][j].Z = R * sin(theta) * cos(phi);
+
+      G[i][j] = Rot(G[i][j], - 200 * sin(phase));
+    }
+  }
+
+  for (i = 0; i < N; i++)
+  {
+    for (j = 0; j < M; j++)
+    {
+      X = Xc + G[i][j].X;
+      Y = Yc - G[i][j].Y;
+      Ellipse(hDC, X - 5, Y - 5, X + 5, Y + 5);
     }
   }
 
     /*Horizontal lines*/
   for (i = 0; i <= N; i++)
   {
-    theta = i * 3.14159265358979323846 / N;
-    x = Xc + R * sin(theta) * cos(phase);
-    y = Yc - R * cos(theta);
-    MoveToEx(hDC,x ,y , NULL);
+    theta = i * PI / N;
+    X = Xc + G[i][0].X;
+    Y = Yc - G[i][0].Y;
+    MoveToEx(hDC, X, Y, NULL);
     for (j = 0; j <= M; j++)
     {
-      phi = j * 2 * 3.14159265358979323846 / M + phase;
-      x = Xc + R * sin(theta) * cos(phi);
-      y = Yc - R * cos(theta);
-      LineTo(hDC, x, y);
+      phi = j * 2 * PI / M + phase;
+      X = Xc + G[i][j].X;
+      Y = Yc - G[i][j].Y;
+      LineTo(hDC, X, Y);
     }
   }
 
     /*Vertical lines*/
   for (j = 0; j <= M; j++ )
   {
-    phi = j * 2 * 3.14159265358979323846 / M + phase;
-    x = Xc;
-    y = Yc - R;
-    MoveToEx(hDC, x, y, NULL);
+    phi = j * 2 * PI / M + phase;
+    X = Xc + G[0][j].X;
+    Y = Yc - G[0][j].Y;
+    MoveToEx(hDC, X, Y, NULL);
     for (i = 0; i <= N; i++)
     {
-      theta = i * 3.14159265358979323846 / N; 
-      x = Xc + R * sin(theta) * cos(phi);
-      y = Yc - R * cos(theta);
-      LineTo(hDC, x, y);
+      theta = i * PI / N; 
+      X = Xc + G[i][j].X;
+      Y = Yc - G[i][j].Y;
+      LineTo(hDC, X, Y);
     }
   }
   
